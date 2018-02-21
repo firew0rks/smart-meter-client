00694053f7c5314a548d88a944e05e1d40177110
Chris Le <chrisle@Chriss-MacBook-Pro.local>
2018-02-21 11:09:35 +1100

    grab data from a dummy smart contract and display it on the new frontend using states

--- src/App.js
+++ src/App.js
@@ -34,7 +34,14 @@ class App extends Component {
     this.state = {
       storageValue: 0,
       web3: null,
-      drawerIsOpen: false
+      drawerIsOpen: false,
+      production: 0 + " kWH",
+      efficiency: 0 + " %",
+      current_usage: 0 + " kWH",
+      average_usage: 0 + "kWH",
+      amount_spent_this_month: "$" + 0,
+      amount_saved_this_month: "$" + 0
+
     }
   }
 
@@ -76,37 +83,35 @@ class App extends Component {
 
     powerContract.deployed().then(instance => {
       console.log(instance);
-      instance.getProduction.call().then(production => {
-        console.log(production);
-        console.log(production.toString());
+      instance.getProduction.call().then(data => {
+        this.setState({
+          production: data + " kWH"
+        });
       })
-      instance.getCurrent_usage.call().then(current_usage => {
-        console.log(current_usage);
-        console.log(current_usage.toString());
+      instance.getEfficiency.call().then(data => {
+        this.setState({
+          efficiency: data + " %"
+        });
       })
-      instance.getAverage_usage.call().then(average_usage => {
-        console.log(average_usage);
-        console.log(average_usage.toString());
-    // const contract = require('truffle-contract');
-    // const simpleStorage = contract(SimpleStorageContract);
-    // simpleStorage.setProvider(this.state.web3.currentProvider);
-
-    // // Declaring this for later so we can chain functions on SimpleStorage.
-    // var simpleStorageInstance;
-
-    // // Get accounts.
-    // this.state.web3.eth.getAccounts((error, accounts) => {
-    //   simpleStorage.deployed().then((instance) => {
-    //     simpleStorageInstance = instance;
-
-    //     // Stores a given value, 5 by default.
-    //     return simpleStorageInstance.set(5, {from: accounts[0]})
-    //   }).then((result) => {
-    //     // Get the value from the contract to prove it worked.
-    //     return simpleStorageInstance.get.call(accounts[0])
-    //   }).then((result) => {
-    //     // Update state with the result.
-    //     return this.setState({ storageValue: result.c[0] })
+      instance.getCurrent_usage.call().then(data => {
+        this.setState({
+          current_usage: data + " kWH"
+        });
+      })
+      instance.getAverage_usage.call().then(data => {
+        this.setState({
+          average_usage: data + " kWH"
+        });
+      })
+      instance.getAmount_spent_this_month.call().then(data => {
+        this.setState({
+          amount_spent_this_month: "$" + data
+        });
+      })
+      instance.getAmount_saved_this_month.call().then(data => {
+        this.setState({
+          amount_saved_this_month: "$" + data
+        });
       })
     })
 
@@ -150,10 +155,10 @@ class App extends Component {
                   <i className="fas fa-sun fa-8x" style={{color: '#E2D58B'}}></i>
                 </Grid>
                 <Grid item xs={6}>
-                    <ColumnDisplay label={'Production'} number={'1,000 kWh'} />
+                    <ColumnDisplay label={'Production'} number={this.state.production}/>
                 </Grid>
                 <Grid item xs={3}>
-                  <ColumnDisplay label={'Efficiency'} number={'29%'}/>
+                  <ColumnDisplay label={'Efficiency'} number={this.state.efficiency}/>
                 </Grid>
               </Grid>
 
@@ -170,7 +175,7 @@ class App extends Component {
           <Grid container style={{height: 50}}/>
           <Grid container style={{height: 300}}>
             <Grid item xs={12}>
-              <RowDisplay label={'Curent Usage'} text={'1,576 kWh'}/>
+              <RowDisplay label={'Curent Usage'} text={this.state.current_usage}/>
             </Grid>
             <Grid item xs={12}>
               <RowDisplay label={'Average Usage'} text={'1,247 kWh'}/>
@@ -193,10 +198,10 @@ class App extends Component {
 
               <Grid container direction={'column'} style={{padding: '20px 20px 20px 30px'}}>
                 <Grid item xs={12}>
-                  <ColumnDisplay label={'Amount Spent this month'} number={'$2,382'} />
+                  <ColumnDisplay label={'Amount Spent this month'} number={this.state.amount_spent_this_month} />
                 </Grid>
                 <Grid item xs={12}>
-                  <ColumnDisplay label={'Amount saved this month'} number={'$273'} />
+                  <ColumnDisplay label={'Amount saved this month'} number={this.state.amount_saved_this_month} />
                 </Grid>
               </Grid>
             </Grid>
