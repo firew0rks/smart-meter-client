f71d08721fb156d8db146e20413f2633f6127261
nathanaelGithub <nat11public@gmail.com>
2018-02-21 20:42:47 +1100

    landing page button

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react'
-// import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-import getWeb3 from './utils/getWeb3'
-import Power from '../build/contracts/Power.json'
-import SwitchLabels from './SwitchLabels'
 import AppBarExampleIcon from './AppBarExampleIcon'
 
 import './css/oswald.css'
@@ -53,10 +49,10 @@ class LandingPage extends Component {
     <Grid container style={{padding: 30, height: 700, justifyContent: 'center', marginTop: 30}}>
         <Grid container direction={'row'} style={{
             justifyContent: 'center', 
-            marginTop:130, 
+            marginTop:150, 
             padding: '10px 5px 5px 20px', 
             width: '100%', 
-            height:350,
+            height:250,
             color: '#DCEED1', 
             fontSize: 95,
             fontFamily: "Roboto", 
@@ -86,41 +82,40 @@ class LandingPage extends Component {
             fontWeight: 'bold'
             }}>
             <Grid item > 
-                <Grid>
+            {/* <Button label="Default" /> */}
+            <Button
+                label="Choose an Image"
+                labelPosition="before"
+                style={styles.uploadButton}
+                containerElement="label"
+                >
+                <input type="file" style={styles.uploadInput} />
+            </Button>
+            {/* <Button
+                label="Label before"
+                labelPosition="before"
+                primary={true}
+                {/* icon={<ActionAndroid />} */}
+            {/*/> */}
+            {/* <Button
+                href="https://github.com/callemall/material-ui"
+                target="_blank"
+                label="GitHub Link"
+                secondary={true}
+                icon={<FontIcon className="muidocs-icon-custom-github" />}
+            /> */}
+                
+                {/* <Button disabled href="/" className={button}>
+                VIEW MY POWER CHAIN (Link disabled)
+                </Button> */}
+                {/* <Grid>
                     <b style={{color: '#DCEED1'}}>VIEW MY POWER CHAIN</b>
-                </Grid>
+                </Grid> */}
             </Grid>
         </Grid>
     </Grid>
-
-        {/* <Grid item xs={12} style={{justifyContent: 'center', paddingRight: 25, marginTop: 300}}>
-          {/* <Grid container direction={'column'}> */}
-            {/* <Grid item xs={12}>
-              <h1>DASHBOARD</h1>
-            </Grid> */}
-          {/* </Grid> */}
-
-            {/* <Grid container direction={'row'}> */}
-            {/* <Grid item xs={12}>
-                <h2>CLICKME</h2> */}
-
-                {/*</Grid>
-                <Grid item xs={4}>
-                  
-                </Grid>
-                <Grid item xs={3} >
-                <div style={{display: 'flex', justifyContent: 'center', marginTop: 40}}>
-                    <b style={{fontSize: 40, color: '#DCEED1', marginRight: 10 }}>7238</b>
-                    <img src="/images/logo.png" style={{height: 40}}/>
-                  </div> */}
-
-            {/* </Grid>
-            </Grid> */} 
             
-    {/* </Grid>
-    </Grid> */}
-            
-
+    {/* DRAWER */}
       <Drawer open={this.state.drawerIsOpen} anchor="right" classes={{root: {color: '#4C5760'}}}>
         <Button onClick={() => this.toggleDrawer()} style={{color: 'grey'}}>
           Menu
